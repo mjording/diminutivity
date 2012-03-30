63be39a446f12554befae4049116e95fd977b476
Matthew Jording <mjording@opengotham.com>
2012-03-30 15:18:22 -0400

    added michael with proper diminutives

--- lib/diminutivity.rb
+++ lib/diminutivity.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'pathname'
 
 module Diminutivity
-  VERSION = "0.0.6"
+  VERSION = "0.0.7"
   NAMEFILE = "lib/names1.1.csv"
   class Matcher
     attr_accessor :names,:namefile
