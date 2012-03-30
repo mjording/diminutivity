02685308b6772637da3d06a5e79a99c0a83e866d
Matthew Jording <mjording@opengotham.com>
2012-03-30 18:57:21 -0400

    added jeff as jeffery

--- lib/diminutivity.rb
+++ lib/diminutivity.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'pathname'
 
 module Diminutivity
-  VERSION = "0.0.15"
+  VERSION = "0.0.16"
   NAMEFILE = "lib/names1.1.csv"
   class Matcher
     attr_accessor :names,:namefile
