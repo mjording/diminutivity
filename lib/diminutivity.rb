781d86638e3069f9cd8c55312aacada4149465ba
Matthew Jording <mjording@opengotham.com>
2012-03-30 16:49:19 -0400

    added nicki as diminutive for nicole

--- lib/diminutivity.rb
+++ lib/diminutivity.rb
@@ -2,7 +2,7 @@ require 'csv'
 require 'pathname'
 
 module Diminutivity
-  VERSION = "0.0.10"
+  VERSION = "0.0.11"
   NAMEFILE = "lib/names1.1.csv"
   class Matcher
     attr_accessor :names,:namefile
